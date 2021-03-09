33326b4ea1205bf55ec817c13d3352ac2de29ddc
LimingGao <winstonglm@gmail.com>
2021-03-09 18:20:36 -0500

    Revert "Add 'PathClassLibrary/' from commit 'c2166d884ace5cfe700ed1ab264e3af673377266'"
    This reverts commit e2b732c3d37b27cdd0d6deea219a6264c72b5382, reversing
    changes made to 9b58b3352260c2ac6a7bbefa44be5328e507efc4.

--- PathClassLibrary/OLD_CODE_TO_ADD/Code/script_determineSeparationPoints.m
+++
@@ -1,9 +0,0 @@
-clear, clc
-% Plot out the routes and determine the starting and ending index
-load("IntersectionGPSData.mat");
-start_idx = 1;
-break_idx = 100000;
-latitude = GPS.Latitude(start_idx:break_idx);
-longitude = GPS.Longitude(start_idx:break_idx);
-
-plot(longitude, latitude)
--- PathClassLibrary/README.md
+++
@@ -1,2 +0,0 @@
-# PathPlanning_PathTools_PathClassLibrary
-This is the Path class library for MATLAB and listing of all functions within this class.
