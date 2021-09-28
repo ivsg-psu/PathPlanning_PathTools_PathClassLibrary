e0509803f5cdacf39aae8f9b59dcb8591f063d47
Stephen J. Harnett <sjh6473@arl.psu.edu>
2021-09-28 13:54:49 -0400

    updating run all wrapper

--- Functions/script_test_all_functions.m
+++ Functions/script_test_all_functions.m
@@ -1,31 +1,15 @@
-% script_test_all_functions
-% This is a script to exercise all functions in the path class
-% This function was written on 2020_10_10 by S. Brennan, sbrennan@psu.edu
-
-% Revision history:
-%     2021_01_09
-%     -- first write of the code
-
-flag_file_log = 0;
-
-all_scripts = dir('script_test_fcn_*');
+% This is a wrapper script to run all the test scripts in the path
+% class library for the purpose of evaluating every assertion test in these
+% files
+clear all; close all; clc;
+all_scripts = dir('script_test_fcn_*.m');
+diary 'script_test_fcn_path_all_stdout.txt';
 for i_script = 1:length(all_scripts)
-    script_name = all_scripts(i_script).name;
-    
-    clc
-    close all
-    
-    if flag_file_log
-        fid = fopen('test_log.txt','a');  %#ok<*UNRCH> % Open file in append mode
-        fprintf(fid,'Testing script: %s\n',script_name);
-        fclose(fid);
+    file_name_extended = all_scripts(i_script).name;
+    file_name = erase(file_name_extended,'.m');
+    if ~strcmp(mfilename,file_name)
+        file_name_trunc = erase(file_name,'script_');
+        suite = testsuite(file_name);
+        results = run(suite);
     end
-    
-    % Evaluate each script name, dropping the .m at the end
-    eval(script_name(1:end-2));
-
 end
-
-close all;
-clc;
-fprintf(1,'All files tested - none threw errors.\n');
